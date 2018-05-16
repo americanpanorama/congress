02c84b3bea8e5a07bfb4ae0c57e59661eebda8f0
rnelson2 <rnelson2@richmond.edu>
2018-05-16 09:01:26 -0400

    refactored based on linting

--- src/components/District.jsx
+++ src/components/District.jsx
@@ -1,40 +1,38 @@
 import * as React from 'react';
-
-import ReactDOM from 'react-dom';
-import * as d3 from "d3"
-import DimensionsStore from '../stores/DimensionsStore.js';
+import PropTypes from 'prop-types';
+import * as d3 from 'd3';
 
 export default class District extends React.Component {
-
   constructor (props) {
     super(props);
     this.state = {
       color: this.props.fill,
       stroke: this.props.stroke,
       fillOpacity: this.props.fillOpacity,
-      strokeOpacity: this.props.strokeOpacity,
-      windowDimensions: DimensionsStore.getMapDimensions()
+      strokeOpacity: this.props.strokeOpacity
     };
+
+    this.district = React.createRef();
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.fill !== nextProps.fill || this.props.stroke !== this.props.stroke || this.props.fillOpacity !== nextProps.fillOpacity || this.props.strokeOpacity !== nextProps.strokeOpacity ) {
-      let duration = this.props.duration || 2000;
-
-      d3.select(ReactDOM.findDOMNode(this))
+  componentWillReceiveProps (nextProps) {
+    if (this.props.fill !== nextProps.fill ||
+     this.props.stroke !== nextProps.stroke ||
+     this.props.fillOpacity !== nextProps.fillOpacity ||
+     this.props.strokeOpacity !== nextProps.strokeOpacity) {
+      d3.select(this.district.current)
         .transition()
         .ease(d3.easeSin)
-        .duration(duration)
+        .duration(this.props.duration)
         .style('fill', nextProps.fill)
         .style('stroke', nextProps.stroke)
         .style('fill-opacity', nextProps.fillOpacity)
         .style('stroke-opacity', nextProps.strokeOpacity)
         .on('end', () => {
           this.setState({
-            fill: nextProps.fill,
+            color: nextProps.fill,
             fillOpacity: nextProps.fillOpacity,
-            strokeOpacity: nextProps.strokeOpacity,
-            //windowDimensions: DimensionsStore.getMapDimensions()
+            strokeOpacity: nextProps.strokeOpacity
           });
         });
     }
@@ -43,21 +41,43 @@ export default class District extends React.Component {
   render () {
     return (
       <path
-        d={ this.props.d }
-        fill={ this.state.color }
-        fillOpacity={ this.state.fillOpacity }
-        stroke={ this.state.stroke }
-        strokeWidth={ this.props.strokeWidth }
-        strokeOpacity={ this.state.strokeOpacity }
-        onClick={ this.props.onDistrictSelected }
-        onMouseEnter={(this.props.onDistrictInspected) ? this.props.onDistrictInspected : () => false}
-        onMouseOver={(this.props.onDistrictInspected) ? this.props.onDistrictInspected : () => false}
-        
-        onMouseLeave={(this.props.onDistrictInspected) ? this.props.onDistrictUninspected : () => false}
+        d={this.props.d}
+        fill={this.state.color}
+        fillOpacity={this.state.fillOpacity}
+        stroke={this.state.stroke}
+        strokeWidth={this.props.strokeWidth}
+        strokeOpacity={this.state.strokeOpacity}
+        onClick={this.props.onDistrictSelected}
+        onMouseEnter={this.props.onDistrictInspected}
+        onMouseOver={this.props.onDistrictInspected}
+        onFocus={this.props.onDistrictInspected}
+        onMouseLeave={this.props.onDistrictUninspected}
         id={this.props.id}
         style={{ pointerEvents: this.props.pointerEvents }}
-        
+        ref={this.district}
       />
     );
   }
 }
+
+District.propTypes = {
+  d: PropTypes.string.isRequired,
+  fill: PropTypes.string.isRequired,
+  stroke: PropTypes.string.isRequired,
+  fillOpacity: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
+  strokeOpacity: PropTypes.number.isRequired,
+  strokeWidth: PropTypes.number.isRequired,
+  pointerEvents: PropTypes.string,
+  onDistrictSelected: PropTypes.func.isRequired,
+  onDistrictInspected: PropTypes.func,
+  onDistrictUninspected: PropTypes.func,
+  duration: PropTypes.number
+};
+
+District.defaultProps = {
+  onDistrictInspected: () => false,
+  onDistrictUninspected: () => false,
+  pointerEvents: 'none',
+  duration: 2000
+};
