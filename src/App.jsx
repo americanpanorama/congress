87d1558f5f84c841586025b962b0b2c59039beb2
rnelson2 <rnelson2@richmond.edu>
2018-05-22 17:55:58 -0400

    reworked how zooming and panning works on the map

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,46 @@
 // import node modules
 import * as React from 'react';
 
-import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
-import ToggleButton from 'react-toggle-button'
-
 // utils
 import { AppActions, AppActionTypes } from './utils/AppActionCreator';
-import AppDispatcher from './utils/AppDispatcher';
-import { getColorForParty, getColorForMargin, yearForCongress, congressForYear, getStateName, ordinalSuffixOf } from './utils/HelperFunctions';
+import { getColorForParty, congressForYear, getStateName, ordinalSuffixOf } from './utils/HelperFunctions';
 
-import Bubble from './components/Bubble.jsx';
-import Context from './components/Context.jsx';
-import District from './components/District.jsx';
-import StateDistGraph from './components/StateDistGraph.jsx';
-import Timeline from './components/Timeline.jsx';
+import TheMap from './components/Map';
+import Context from './components/Context';
+import Timeline from './components/Timeline';
 import TimelineHandle from './components/TimelineHandle';
-import DistrictTimeline from './components/DistrictTimeline.jsx';
-import ZoomControls from './components/ZoomControls.jsx';
-import MapLegend from './components/MapLegend.jsx';
-
 
 import DistrictsStore from './stores/Districts';
 import DimensionsStore from './stores/DimensionsStore';
 import TextStore from './stores/Text';
 import HashManager from './stores/HashManager';
 
-import StatesTopoJson from '../data/states.json';
-
 // main app container
 class App extends React.Component {
-
   constructor (props) {
     super(props);
 
     // initialize state
     const theHash = HashManager.getState();
-    const [x,y,z] = (theHash.xyz) ? theHash.xyz.split('/') : [0,0,1];
+    const [x, y, z] = (theHash.xyz) ? theHash.xyz.split('/') : [0, 0, 1];
     this.state = {
-      selectedYear: parseInt(theHash.year) || 1952,
-      selectedView: theHash.view || 'cartogram',
-      selectedParty: null,
+      selectedYear: parseInt(theHash.year, 10) || 1952,
       selectedDistrict: theHash.district || null,
+      selectedParty: null,
       onlyFlipped: false,
       inspectedDistrict: null,
-      winnerView: theHash.show == 'winner',
-      dorling: theHash.view != 'map',
+      dorling: theHash.view !== 'map',
       zoom: z,
       x: x,
-      y: y,
+      y: y
     };
 
     // bind handlers
-    const handlers = ['onWindowResize','onYearSelected', 'onViewSelected', 'toggleDorling', 'toggleView', 'storeChanged', 'onZoomIn', 'zoomOut', 'resetView', 'handleMouseUp', 'handleMouseDown', 'handleMouseMove', 'onDistrictInspected', 'onDistrictUninspected', 'onDistrictSelected', 'onPartySelected','toggleFlipped','zoomToBounds','dimensionsChanged','onModalClick'];
-    handlers.forEach(handler => { this[handler] = this[handler].bind(this); });
+    const handlers = ['onWindowResize', 'onYearSelected', 'toggleDorling', 'storeChanged', 'onZoomIn', 'zoomOut', 'resetView', 'handleMouseUp', 'handleMouseDown', 'handleMouseMove', 'onDistrictInspected', 'onDistrictUninspected', 'onDistrictSelected', 'onPartySelected', 'toggleFlipped', 'zoomToBounds', 'dimensionsChanged', 'onModalClick'];
+    handlers.forEach((handler) => { this[handler] = this[handler].bind(this); });
   }
 
-  componentWillMount () { 
-    //const theHash = HashManager.getState();
+  componentWillMount () {
     AppActions.loadInitialData(this.state, HashManager.getState());
   }
 
@@ -66,21 +49,16 @@ class App extends React.Component {
     DistrictsStore.addListener(AppActionTypes.storeChanged, this.storeChanged);
     TextStore.addListener(AppActionTypes.storeChanged, this.storeChanged);
     DimensionsStore.addListener(AppActionTypes.storeChanged, this.dimensionsChanged);
-    // this.setState({
-    //   x: DimensionsStore.getDimensions().mapWidth/2,
-    //   y: DimensionsStore.getDimensions().mapHeight/2,
-    // });
   }
 
   componentDidUpdate () { this.changeHash(); }
 
-  storeChanged() { this.setState({}); }
+  storeChanged () { this.setState({}); }
 
   dimensionsChanged() {
-    console.log(DimensionsStore.getMapDimensions().height);
     this.setState({
-      x: DimensionsStore.getMapDimensions().width  / 2 - (DimensionsStore.getMapDimensions().width  / 2 - this.state.x) / this.state.zoom,
-      y: DimensionsStore.getMapDimensions().height  / 2 - (DimensionsStore.getMapDimensions().height  / 2 - this.state.y) / this.state.zoom
+      x: DimensionsStore.getMapDimensions().width / 2 - (DimensionsStore.getMapDimensions().width / 2 - this.state.x) / this.state.zoom,
+      y: DimensionsStore.getMapDimensions().height / 2 - (DimensionsStore.getMapDimensions().height / 2 - this.state.y) / this.state.zoom
     });
   }
 
@@ -100,12 +78,12 @@ class App extends React.Component {
         this.setState({ 
           selectedYear: year,
           selectedDistrict: selectedDistrict
-        }); 
+        });
       }
     }, 50);
   }
 
-  onPartySelected(e) { 
+  onPartySelected (e) { 
     let selectedParty;
     if (e && e.currentTarget) {
       selectedParty = (e.currentTarget.id == this.state.selectedParty) ? null : e.currentTarget.id;
@@ -115,22 +93,22 @@ class App extends React.Component {
 
     this.setState({ selectedParty: selectedParty }); 
   }
-
-  onDistrictInspected(e) {
+ 
+  onDistrictInspected (e) {
     this.setState({ inspectedDistrict: e.target.id }); 
   }
 
-  onDistrictUninspected() {
+  onDistrictUninspected () {
     this.setState({ inspectedDistrict: null }); 
   }
 
-  onDistrictSelected(e) {
+  onDistrictSelected (e) {
     const id = (e.currentTarget.id !== this.state.selectedDistrict) ? e.currentTarget.id : null;
     this.setState({ selectedDistrict: id }); 
   }
 
-  toggleFlipped(newState) {
-    newState = (typeof newState == 'boolean') ? newState : !this.state.onlyFlipped;
+  toggleFlipped (newState) {
+    newState = (typeof newState === 'boolean') ? newState : !this.state.onlyFlipped;
     this.setState({ onlyFlipped: newState }); 
   }
 
@@ -139,15 +117,7 @@ class App extends React.Component {
     AppActions.onModalClick(subject);
   }
 
-  onViewSelected(e) {
-    const selectedView = (this.state.selectedView == 'map') ? 'cartogram' : 'map';
-    this.setState({ 
-      selectedView: selectedView,
-      dorling: (selectedView == 'map') ? false : (selectedView == 'cartogram') ? true : this.state.dorling,
-    });
-  }
-
-  onZoomIn(event) {
+  onZoomIn (event) {
     event.preventDefault();
     const z = Math.min(this.state.zoom * 1.62, 18),
       centerX = (event.currentTarget.id == 'zoomInButton') ? DimensionsStore.getMapDimensions().width  / 2 - this.state.x : event.nativeEvent.offsetX - this.state.x,
@@ -229,8 +199,6 @@ class App extends React.Component {
     });
   }
 
-  toggleView(e) { this.setState({ winnerView: !this.state.winnerView }); }
-
   toggleDorling() { 
     this.setState({ dorling: !this.state.dorling }); 
   }
@@ -238,10 +206,7 @@ class App extends React.Component {
   changeHash () {
     const vizState = { 
       year: this.state.selectedYear,
-      view: this.state.selectedView,
-      district: this.state.selectedDistrict,
-      xyz: [this.state.x, this.state.y, this.state.zoom].join('/'),
-      show: (this.state.winnerView) ? 'winner' : 'strength'
+      district: this.state.selectedDistrict
     };
 
     HashManager.updateHash(vizState);
@@ -283,120 +248,13 @@ class App extends React.Component {
           </nav>
         </header>
 
-        {/* map */}
-        <svg 
-          width={ DimensionsStore.getDimensions().mapWidth }
-          height={ DimensionsStore.getDimensions().mapHeight }
-          className='theMap'
-        >
-
-          <filter id="glow" x="-50%" y="-10%" width="200%" height="160%">
-            <feGaussianBlur stdDeviation="10" result="glow"/>
-          </filter>
-
-          <filter id="blur" x="-50%" y="-10%" width="200%" height="160%">
-            <feGaussianBlur stdDeviation="5" result="blur"/>
-          </filter>
-
-          <g
-            { ...DimensionsStore.getMapDimensions() }
-            onDoubleClick={ this.onZoomIn }
-            onMouseUp={this.handleMouseUp }
-            onMouseDown={this.handleMouseDown }
-            onMouseMove={this.handleMouseMove }
-            transform={ 'translate(' + this.state.x + ' ' + this.state.y + ') scale(' + this.state.zoom + ')' }
-          >
-
-            {/* district polygons */}
-            { DistrictsStore.getElectionDistricts(this.state.selectedYear).map(d => 
-              <District
-                d={ DistrictsStore.getPath(d.the_geojson) }
-                key={ 'polygon' + d.id }
-                fill={ (this.state.winnerView || this.state.selectedView =='cartogram') ? getColorForParty(d.regularized_party_of_victory) : getColorForMargin(d.regularized_party_of_victory, d.percent_vote) }
-                //fillOpacity={ (this.state.selectedParty && this.state.selectedParty !== d.regularized_party_of_victory) ? 0.05 : (this.state.selectedView =='cartogram') ? 0.1 : 1 }
-                fillOpacity={ (this.state.selectedParty && this.state.selectedParty !== d.regularized_party_of_victory) ? 0.05 : ((this.state.selectedView == 'cartogram' && viewableDistrict !== d.id) || (viewableDistrict && viewableDistrict !== d.id) || (this.state.onlyFlipped && !d.flipped)) ? 0.1 : 1 }
-                stroke={ '#eee' }
-                strokeWidth={(!viewableDistrict || viewableDistrict != d.id) ? 0.5 : 2 }
-                strokeOpacity={(this.state.selectedView =='cartogram' && (!viewableDistrict || viewableDistrict !== d.id)) ? 0.00 : (viewableDistrict && viewableDistrict !== d.id) ? 0.2 : 1}
-                selectedView={ this.state.selectedView }
-                onDistrictInspected={ this.onDistrictInspected }
-                onDistrictUninspected={ this.onDistrictUninspected }
-                onDistrictSelected={ this.onDistrictSelected }
-                id={d.id}
-                duration={ transitionDuration }
-                pointerEvents={ (this.state.selectedView == 'map') ? 'auto' : 'none'}
-              />
-            )}
-
-            {/* state polygons */}
-            { DistrictsStore.getStates(this.state.selectedYear).map(s =>
-              <path
-                d={ DistrictsStore.getPath(s.geometry) }
-                fill='transparent'
-                stroke={ '#eee'}
-                strokeOpacity={ (this.state.selectedView =='cartogram') ? 0.2 : 1 }
-                strokeWidth={ (!viewableDistrict || s.properties.abbr_name == DistrictsStore.getElectionDataForDistrict(this.state.selectedYear, viewableDistrict).state) ? 1.5 : 0.3 }
-                key={'stateBoundaries'+s.properties.name}
-                filter={ (this.state.selectedView == 'cartogram') ? 'url(#blur)' : '' }
-                style={{ pointerEvents: 'none' }}
-              />
-            )}
-
-            {/* city bubbles */}
-            { DistrictsStore.getBubbleCoords(this.state.selectedYear).cities.map((d, i) => 
-              <Bubble
-                cx={(this.state.dorling) ? d.x : d.xOrigin }
-                cy={(this.state.dorling) ? d.y : d.yOrigin }
-                r={(this.state.dorling) ? d.r : 0.01 }
-                cityLabel={ d.id } 
-                color='#11181b'
-                fillOpacity={0.5}
-                stroke={ (this.state.selectedView == 'map') ? 'transparent' : 'transparent' }
-                key={d.id || 'missing' + i}
-                id={d.id}
-              />
-            )}
-
-            {/* district bubbles */}
-            { DistrictsStore.getBubbleCoords(this.state.selectedYear).districts.map(d=> 
-              <Bubble
-                cx={(this.state.dorling) ? d.x : d.xOrigin }
-                cy={(this.state.dorling) ? d.y : d.yOrigin }
-                r={ DimensionsStore.getDimensions().districtR }
-                color={ (this.state.selectedView == 'map') ? 'transparent' : (this.state.winnerView) ? getColorForParty(d.regularized_party_of_victory) : getColorForMargin(d.regularized_party_of_victory, d.percent_vote)}
-                stroke={ (this.state.selectedView == 'map' || (this.state.selectedParty && this.state.selectedParty !== d.regularized_party_of_victory)) ? 'transparent' : (this.state.selectedView == 'cartogram' && viewableDistrict && viewableDistrict == d.districtId) ? 'white' : getColorForParty(d.regularized_party_of_victory) }
-                fillOpacity={ ((this.state.selectedParty && this.state.selectedParty !== d.regularized_party_of_victory) || (this.state.onlyFlipped && !d.flipped)) ? 0.05 : (viewableDistrict && viewableDistrict !== d.districtId) ? 0.3 : 1 }
-                label={ (d.flipped && ((!this.state.selectedParty || this.state.selectedParty == d.regularized_party_of_victory) && (!this.state.selectedDistrict || d.districtId == this.state.selectedDistrict))) ? 'F' : ''}
-                labelColor={ getColorForParty(d.regularized_party_of_victory) }
-                key={d.id}
-                id={d.districtId}
-                pointerEvents={ (this.state.selectedView == 'map') ? 'none' : 'auto' }
-                selectedView={ this.state.selectedView }
-                onDistrictInspected={ this.onDistrictInspected }
-                onDistrictUninspected={ this.onDistrictUninspected }
-                onDistrictSelected={ this.onDistrictSelected }
-              />
-            )} 
-
-          </g>
-        </svg>
-
-        <MapLegend
-          selectedView={this.state.selectedView}
+        <TheMap
           selectedYear={this.state.selectedYear}
-          selectedParty={this.state.selectedParty}
-          onPartySelected={this.onPartySelected}
-          winnerView={this.state.winnerView}
           onlyFlipped={this.state.onlyFlipped}
-          toggleFlipped={this.toggleFlipped}
-        />
-
-        <ZoomControls
-          onZoomIn={ this.onZoomIn }
-          onZoomOut={ this.zoomOut }
-          resetView={ this.resetView }
-          currentZoom={ this.state.zoom }
-          dimensions={DimensionsStore.getDimensions()}
+          viewableDistrict={this.state.inspectedDistrict || this.state.selectedDistrict}
+          onDistrictInspected={this.onDistrictInspected}
+          onDistrictUninspected={this.onDistrictUninspected}
+          onDistrictSelected={this.onDistrictSelected}
         />
 
         <aside
@@ -440,127 +298,6 @@ class App extends React.Component {
               DistrictsStore.getMaxBottomOffset()]}
           />
         </aside>
-
-
-
-        <div 
-          id='vizControl' 
-          style={{
-            right: DimensionsStore.getDimensions().vizControlsRight,
-            fontSize: DimensionsStore.getDimensions().headerSubtitleFontSize,
-            verticalAlign: 'top',
-            lineHeight: '1em'
-          }}
-        >
-          <span style={{ 
-              color: (this.state.selectedView == 'cartogram') ? '#F0B67F' : '#aaa',
-              verticalAlign: 'top',
-              lineHeight: '1em'
-            }} 
-            onClick={ this.onViewSelected }
-          >
-            Cartogram
-          </span>
-          <ToggleButton
-            value={ this.state.selectedView == 'map' }
-            onToggle={ this.onViewSelected }
-            inactiveLabel={''}
-            activeLabel={''}
-            containerStyle={{
-              display:'inline-block',
-              //transform: 'translateY(5px)',
-              width: DimensionsStore.getDimensions().vizControlTrackHeight * 2,
-              marginRight: 5,
-              marginLeft: 5,
-            }} 
-            colors={{
-              activeThumb: { base: '#eee' },
-              inactiveThumb: { base: '#eee' },
-              active: { base: '#777' },
-              inactive: { base: '#777' }
-            }}
-            activeLabelStyle={{ fontSize: 1 }}
-            inactiveLabelStyle={{ fontSize: 1 }}
-            trackStyle={{
-              height: DimensionsStore.getDimensions().vizControlTrackHeight,
-              width: DimensionsStore.getDimensions().vizControlTrackHeight * 2
-            }}
-            thumbStyle={{
-              height: DimensionsStore.getDimensions().vizControlTrackHeight ,
-              width: DimensionsStore.getDimensions().vizControlTrackHeight,
-            }}
-            thumbAnimateRange={[0, DimensionsStore.getDimensions().vizControlTrackHeight]}
-          />
-          <span 
-            style={{ 
-              color: (this.state.selectedView == 'map') ? '#F0B67F' : '#aaa',
-              verticalAlign: 'top',
-              lineHeight: '1em'
-            }} 
-            onClick={ this.onViewSelected }
-          >
-            Map
-          </span>
-        </div>
-
-        <div 
-          id='winnerControl'
-          style={{
-            left: DimensionsStore.getDimensions().winnerControlLeft,
-            fontSize: DimensionsStore.getDimensions().headerSubtitleFontSize,
-            verticalAlign: 'top',
-            lineHeight: '1em'
-          }}
-        >
-          <span 
-            style={{ 
-              color: (this.state.winnerView) ? '#F0B67F' : '#aaa',
-              fontSize: DimensionsStore.getDimensions().headerSubtitleFontSize,
-              verticalAlign: 'top',
-              lineHeight: '1em'
-            }} 
-            onClick={ this.toggleView }
-          >
-            Winner </span>
-          <ToggleButton
-            value={ !this.state.winnerView }
-            onToggle={ this.toggleView }
-             inactiveLabel={''}
-            activeLabel={''}
-            containerStyle={{
-              display:'inline-block',
-              //transform: 'translateY(5px)',
-              width: DimensionsStore.getDimensions().vizControlTrackHeight * 2,
-              marginRight: 5,
-              marginLeft: 5,
-            }} 
-            colors={{
-              activeThumb: { base: '#eee' },
-              inactiveThumb: { base: '#eee' },
-              active: { base: '#777' },
-              inactive: { base: '#777' }
-            }}
-            activeLabelStyle={{ fontSize: 1 }}
-            inactiveLabelStyle={{ fontSize: 1 }}
-            trackStyle={{
-              height: DimensionsStore.getDimensions().vizControlTrackHeight,
-              width: DimensionsStore.getDimensions().vizControlTrackHeight * 2
-            }}
-            thumbStyle={{
-              height: DimensionsStore.getDimensions().vizControlTrackHeight ,
-              width: DimensionsStore.getDimensions().vizControlTrackHeight,
-            }}
-            thumbAnimateRange={[0, DimensionsStore.getDimensions().vizControlTrackHeight]}
-          />
-          <span 
-            style={{ 
-              color: (!this.state.winnerView) ? '#F0B67F' : '#aaa',
-              fontSize: DimensionsStore.getDimensions().headerSubtitleFontSize,
-              verticalAlign: 'top',
-              lineHeight: '1em'
-            }} 
-            onClick={ this.toggleView }> Strength of Victory</span>
-        </div>
 
         <div
           id='electionLabel'
